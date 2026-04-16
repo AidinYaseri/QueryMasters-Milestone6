87b82ae8ec6aa0e92f0be4aaac8e9438f6880abe
Gab <196218256+Gabo616@users.noreply.github.com>
2026-04-16 11:06:51 -0400

    Created first time user window

--- CalendarAppWPF/CalendarAppWPF/obj/Debug/net8.0-windows/CalendarAppWPF_ik4htl5a_wpftmp.AssemblyInfo.cs
+++ CalendarAppWPF/CalendarAppWPF/obj/Debug/net8.0-windows/CalendarAppWPF_ik4htl5a_wpftmp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalendarAppWPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26b417eb96e3a363987e4e048688b557f5e901f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a25a73b44863957f2bf4d68dcd95f5f8473a82")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalendarAppWPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalendarAppWPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
