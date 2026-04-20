82bbeaf1b594cdfe6292a20b8dcf62834c43959a
Gabriel Moreno-Batista <196218256+Gabo616@users.noreply.github.com>
2026-04-20 16:48:00 -0400

    Designed a custom loading animation, if needed.

---
+++ CalendarAppWPF/CalendarAppWPF/Loading.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace CalendarAppWPF
+{
+    public partial class Loading : Window
+    {
+        private DispatcherTimer dotsTimer;
+        private int dotCount = 0;
+
+        public Loading()
+        {
+            InitializeComponent();
+
+            StartBarAnimation();
+            StartDotsAnimation();
+        }
+
+        private void StartBarAnimation()
+        {
+            DoubleAnimation move = new DoubleAnimation
+            {
+                From = -60,
+                To = 220,
+                Duration = TimeSpan.FromSeconds(1.6),
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+            BarTransform.BeginAnimation(TranslateTransform.XProperty, move);
+        }
+
+        private void StartDotsAnimation()
+        {
+            dotsTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(400)
+            };
+
+            dotsTimer.Tick += (s, e) =>
+            {
+                dotCount = (dotCount + 1) % 4;
+                StatusText.Text = "Setting up your calendar" + new string('.', dotCount);
+            };
+
+            dotsTimer.Start();
+        }
+    }
+}
--- CalendarAppWPF/CalendarAppWPF/obj/Debug/net8.0-windows/CalendarAppWPF.AssemblyInfo.cs
+++ CalendarAppWPF/CalendarAppWPF/obj/Debug/net8.0-windows/CalendarAppWPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalendarAppWPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cac5becf401a1ce4b0b6e8cb88751c08e815c913")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a82d0ad57f1824c880c4d6418545e67c1076d887")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalendarAppWPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalendarAppWPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
